2018a1f5f2dd3e40b4031f6f53efbca831a381d3
Mikkel <mtkongsted@gmail.com>
2023-05-05 16:35:24 +0200

    everything

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 import "./style.css";
+
+function main() {
+    let tutorialPoints = 0;
+    const prevButton =
+        document.querySelector<HTMLButtonElement>("#prev-button")!;
+    const nextButton =
+        document.querySelector<HTMLButtonElement>("#next-button")!;
+    nextButton.addEventListener("click", () => {
+        tutorialPoints++;
+    });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    main();
+});
