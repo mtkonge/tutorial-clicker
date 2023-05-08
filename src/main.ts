fdb6cd41b0221d9bbc36e9b12121e9231884d540
Mikkel <mtkongsted@gmail.com>
2023-05-08 15:44:46 +0200

    everything

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import "./style.css";
+function getRandomNumber(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+}
 
 function main() {
+    const tutorialContainer =
+        document.querySelector<HTMLDivElement>("#tutorial-box")!;
     let tutorialPoints = 0;
     const prevButton =
         document.querySelector<HTMLButtonElement>("#prev-button")!;
@@ -8,6 +13,10 @@ function main() {
         document.querySelector<HTMLButtonElement>("#next-button")!;
     nextButton.addEventListener("click", () => {
         tutorialPoints++;
+        const randomTop = getRandomNumber(0, window.innerWidth / 2);
+        const randomLeft = getRandomNumber(0, window.innerHeight / 2);
+        tutorialContainer.style.top = randomTop + "px";
+        tutorialContainer.style.left = randomLeft + "px";
     });
 }
 
